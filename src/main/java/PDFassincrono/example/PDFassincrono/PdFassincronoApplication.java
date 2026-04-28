1cc911cb355a96438c80f3576af25a08e2af3d47
Jonathan Rodrigues <jonathan7ar@gmail.com>
2026-04-28 20:37:03 -0300

    feat: initial project setup - Spring Boot + Kafka + PostgreSQL

---
+++ src/main/java/PDFassincrono/example/PDFassincrono/PdFassincronoApplication.java
@@ -0,0 +1,13 @@
+package PDFassincrono.example.PDFassincrono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdFassincronoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdFassincronoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PDFassincrono/example/PDFassincrono/PdFassincronoApplicationTests.java
@@ -0,0 +1,13 @@
+package PDFassincrono.example.PDFassincrono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdFassincronoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
